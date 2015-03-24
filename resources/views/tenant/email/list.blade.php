f312143022efb0e6e6281ff4353f7fe1b15f3bdb
pooja@alucio.com <pooja@alucio.com>
2015-03-24 17:34:35 +0545

    css

--- resources/views/tenant/email/list.blade.php
+++ resources/views/tenant/email/list.blade.php
@@ -4,18 +4,29 @@
     <tbody>
           @foreach($mails as $mail)
           <tr class="e{{$mail->id}}">
-            <td class="small-col"><i class="fa fa-envelope"></i></td>
+            <td class="small-col"><i class="fa fa-envelope"></i>
+              @if($mail->status == 1)  {!!'<i class="fa fa-circle green"></i>'!!} @endif
+               @if($mail->status == 2)  {!!'<i class="fa fa-circle red"></i>'!!} @endif
+                @if($mail->status == 3)  {!!'<i class="fa fa-circle orange"></i>'!!} @endif
+               {{--   @if($mail->status == 4)  {!!'<i class="fa fa-circle"></i>'!!} @endif --}}
+
+
+            </td>
             <td class="name">
              <?php $receiver = $mail->receivers;?>
               <a style="display: block" href="#" data-id="{{$mail->id}}" >
-                    @foreach($receiver as $to)
+                  @foreach($receiver as $to)
                         @if($to->type ==1)
                            {{ str_limit($to->email,30)}};
                         @endif
                      @endforeach
+
+                    
                 <small class="subject">{{str_limit($mail->subject,40)}}</small>
-              </a>
+              </a> 
             </td>
+            <td> @if(!empty($mail['attachments'])) {!!'<i class="fa fa-paperclip grey"></i>'!!} @endif</td>
+            
             <td class="time"><small>{{email_date($mail->created_at)}}</small></td>
           </tr>
           @endforeach
