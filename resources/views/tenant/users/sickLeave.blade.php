58d23b7a384cf2bcffc51701af666967ece1ff27
pooja@alucio.com <pooja@alucio.com>
2015-04-03 17:22:54 +0545

    bill

--- resources/views/tenant/users/sickLeave.blade.php
+++ resources/views/tenant/users/sickLeave.blade.php
@@ -62,29 +62,46 @@
   });
 
 
-  $("#from").datepicker({
-              'format': 'yyyy-mm-dd',
-        onSelect: function(date) {
-          
-            
+ 
+    $(function () {
+       var startDate = new Date('2012-01-01');
+var FromEndDate = new Date();
+var ToEndDate = new Date();
 
-        },  
-        onClose: function( selectedDate ) {
-            $( "#to" ).datepicker( "option", "minDate", selectedDate );
-        } 
+ToEndDate.setDate(ToEndDate.getDate()+365);
 
-         });
- $("#to").datepicker({
-            'format': 'yyyy-mm-dd',
-        onSelect: function(date) {
-           
-            
+$('#from').datepicker({
+    
+    weekStart: 1,
+    startDate: '01/01/2012',
+    //endDate: FromEndDate, 
+     format: 'yyyy-mm-dd',
+    autoclose: true
+})
+    .on('changeDate', function(selected){
+        startDate = new Date(selected.date.valueOf());
+        startDate.setDate(startDate.getDate(new Date(selected.date.valueOf())));
+        $('#to').datepicker('setStartDate', startDate);
+    }); 
+$('#to').datepicker({
+        
+        weekStart: 1,
+        startDate: startDate,
+        endDate: ToEndDate,
+        autoclose: true,
+         format: 'yyyy-mm-dd'
+    })
+    .on('changeDate', function(selected){
+        FromEndDate = new Date(selected.date.valueOf());
+        FromEndDate.setDate(FromEndDate.getDate(new Date(selected.date.valueOf())));
+        $('#from').datepicker('setEndDate', FromEndDate);
+    });
 
-        },  
-        onClose: function( selectedDate ) {
-           
-        } 
-         });
+  
+  
+  
+  
+});
 
   $(document).on('click', '.saveleave', function (e) {
         e.preventDefault();
