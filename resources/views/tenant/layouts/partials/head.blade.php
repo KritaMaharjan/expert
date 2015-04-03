bcee97e3c044ed9adf33c8106cbb94022351fecb
sanjay <sanjay@alucio.com>
2015-04-03 10:24:27 -0700

    stats

--- resources/views/tenant/layouts/partials/head.blade.php
+++ resources/views/tenant/layouts/partials/head.blade.php
@@ -9,6 +9,9 @@
 <link href="{{ asset('assets/css/fonts-awesome.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/styles.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/custom.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}" rel="stylesheet" type="text/css" />
+
 <link href="{{ asset('assets/css/_all-skins.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/select2.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/plugins/datatables/dataTables.bootstrap.css')}}" rel="stylesheet" type="text/css" />
