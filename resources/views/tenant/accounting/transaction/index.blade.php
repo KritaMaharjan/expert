ed7323a01b889c20587b654026412b39830d03e4
sanjay <sanjay@alucio.com>
2015-04-23 14:12:55 -0700

    Transaction

--- resources/views/tenant/accounting/transaction/index.blade.php
+++ resources/views/tenant/accounting/transaction/index.blade.php
@@ -1,11 +1,11 @@
 @extends('tenant.layouts.main')
 
 @section('heading')
-VAT
+Transaction
 @stop
 
 @section('content')
-<div class="box box-solid">
+<!-- <div class="box box-solid">
     <div class="box-body">
         <div class="box-header pad-0">
             <h3 class="box-title">Close Accounting Year</font></h3>
@@ -57,8 +57,78 @@ VAT
                     </div>
                 </div>
             </div>
+        </div> -->
+
+
+        <div class="box box-solid">
+            <div class="box-body">
+                <div class="box-header pad-0">
+                    <h3 class="box-title">Transaction</font></h3>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="no-list-style">
+                            <li>
+                                <div class="accord">
+                                    <p class="accord-list">10.02.2015 - <span><strong>Payment</strong></span> <span><strong>Bill number 10001</strong></span></p>
+                                </div>
+                                <div class="detail-sec table-responsive">
+                        
+                                    <table class="table detail-table">
+                                        <tbody>
+                                        <tr>
+                                            <th>S.N</td>
+                                            <th>Account number</td>
+                                            <th>Name</td>
+                                            <th>Amount</td>
+                                            <th>VAT Code:</td>
+
+                                        </tr>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>2701</td>
+                                            <td>2701 Output value added tax high rate</td>
+                                            <td>Kr 3 750,00</td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>3000</td>
+                                            <td>3000 Sales revenue, high tax rate</td>
+                                            <td>Kr 15 000,00</td>
+                                            <td>Sales high tax</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>1500:10001</td>
+                                            <td>Customer name</td>
+                                            <td>Kr -18 750,00</td>
+                                            <td></td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    
+
+                </div>
+            </div>
         </div>
 
+
+
+        <script type="text/javascript">
+            $(function(){
+                $('.accord-list').click(function(){
+                    $(this).parent().parent().find('.detail-sec').slideToggle();                    
+                
+                });
+            });
+        </script>
 
         {{--Load JS--}}
         {{FB::registerModal()}}
