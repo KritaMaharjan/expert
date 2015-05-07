f3b2a79829b1984f2370fd99794dae90950b8ea8
Krita Maharjan <krita@alucio.com>
2015-05-07 17:51:48 +0545

    datepicker

--- public_html/assets/js/statistics.js
+++ public_html/assets/js/statistics.js
@@ -1,9 +1,6 @@
 var FromEndDate = new Date();
 var startDate = new Date();
-var ToEndDate = new Date();
 var today = new Date();
-
-ToEndDate.setDate(ToEndDate.getDate()+365);
 
 $('#date-frm-date-pickers').datepicker({
     format: "yyyy-mm-dd",
@@ -29,3 +26,26 @@ $('#date-to-date-pickers')
         FromEndDate.setDate(FromEndDate.getDate(new Date(selected.date.valueOf())));
         $('#date-frm-date-pickers').datepicker('setEndDate', FromEndDate);
     });
+
+// LINE CHART
+var line = new Morris.Line({
+    element: 'line-chart',
+    resize: true,
+    data: [
+        {y: '2011 Q1', item1: 2666, item2: 2666, item3: 15666, item4: 13666},
+        {y: '2011 Q2', item1: 2778, item2: 2294, item3: 14666, item4: 12666},
+        {y: '2011 Q3', item1: 4912, item2: 1969, item3: 10066, item4: 9666},
+        {y: '2011 Q4', item1: 3767, item2: 3597, item3: 13066, item4: 11666},
+        {y: '2012 Q1', item1: 6810, item2: 1914, item3: 3566, item4: 12666},
+        {y: '2012 Q2', item1: 5670, item2: 4293, item3: 2696, item4: 9666},
+        {y: '2012 Q3', item1: 4820, item2: 3795, item3: 1666, item4: 12666},
+        {y: '2012 Q4', item1: 15073, item2: 5967, item3: 4666, item4: 11666},
+        {y: '2013 Q1', item1: 10687, item2: 4460, item3: 3666, item4: 6666},
+        {y: '2013 Q2', item1: 8432, item2: 5713, item3: 2866, item4: 7666}
+    ],
+    xkey: 'y',
+    ykeys: ['item1', 'item2', 'item3', 'item4'],
+    labels: ['Customers', 'Billing', 'Collection', 'Statistics'],
+    lineColors: ['#fd9d11','#237a7a','#7070a0','#d45127'],
+    hideHover: 'auto'
+});
--- resources/views/tenant/statistics/index.blade.php
+++ resources/views/tenant/statistics/index.blade.php
@@ -181,37 +181,13 @@ Statistics
   </div>
 </div>
 
-
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="{{ asset('assets/plugins/morris/morris.min.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/sparkline/jquery.sparkline.min.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}" type="text/javascript"></script>
-     <script type="text/javascript">
-      // LINE CHART
-        var line = new Morris.Line({
-          element: 'line-chart',
-          resize: true,
-          data: [
-            {y: '2011 Q1', item1: 2666, item2: 2666, item3: 15666, item4: 13666},
-            {y: '2011 Q2', item1: 2778, item2: 2294, item3: 14666, item4: 12666},
-            {y: '2011 Q3', item1: 4912, item2: 1969, item3: 10066, item4: 9666},
-            {y: '2011 Q4', item1: 3767, item2: 3597, item3: 13066, item4: 11666},
-            {y: '2012 Q1', item1: 6810, item2: 1914, item3: 3566, item4: 12666},
-            {y: '2012 Q2', item1: 5670, item2: 4293, item3: 2696, item4: 9666},
-            {y: '2012 Q3', item1: 4820, item2: 3795, item3: 1666, item4: 12666},
-            {y: '2012 Q4', item1: 15073, item2: 5967, item3: 4666, item4: 11666},
-            {y: '2013 Q1', item1: 10687, item2: 4460, item3: 3666, item4: 6666},
-            {y: '2013 Q2', item1: 8432, item2: 5713, item3: 2866, item4: 7666}
-          ],
-          xkey: 'y',
-          ykeys: ['item1', 'item2', 'item3', 'item4'],
-          labels: ['Customers', 'Billing', 'Collection', 'Statistics'],
-          lineColors: ['#fd9d11','#237a7a','#7070a0','#d45127'],
-          hideHover: 'auto'
-        });
-    </script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+{{ FB::js('assets/plugins/morris/morris.min.js') }}
+{{ FB::js('assets/plugins/morris/morris.min.js') }}
+{{ FB::js('assets/plugins/sparkline/jquery.sparkline.min.js') }}
+{{ FB::js('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}
+{{ FB::js('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}
+{{ FB::js('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}
 
 {{ FB::js('assets/js/statistics.js') }}
 
