4d3ba95439770a37cda0407f614f4672b97f4634
Manish Singh <mgsingh@alucio.com>
2015-05-08 16:36:31 +0545

    case count

--- app/Http/Tenant/Statistics/Repositories/CollectionRepository.php
+++ app/Http/Tenant/Statistics/Repositories/CollectionRepository.php
@@ -55,4 +55,28 @@ class CollectionRepository {
         return $stats;
     }
 
+    function getCasesBySep()
+    {
+
+        $status = [BILL::STATUS_COLLECTION];
+        $select = [DB::raw('MAX(col.step) as step'), DB::raw('MAX(col.created_at) as created_at'), 'b.id', 'c.name as customer_name', 'b.invoice_number', 'b.total as bill_total', 'b.paid', 'b.remaining', DB::raw('DATE_FORMAT(b.due_date,"%Y-%m-%d") as due_date')];
+
+        $step = $this->request->get('step');
+        $step_string = $step == '' ? 'purring' : $step;
+        $step = Collection::getStep($step_string);
+
+        $query = $this->bill->select($select)->from('fb_bill as b')
+            ->leftJoin('fb_customers as c', 'c.id', '=', 'b.customer_id')
+            ->leftJoin('fb_collection as col', 'col.bill_id', '=', 'b.id')
+            ->whereIn('b.status', $status)// get bills having collection status
+            ->where('b.payment', '!=', BILL::STATUS_PAID)// get only unpaid bill
+            ->where('b.is_offer', BILL::TYPE_BILL)// get only bill type
+            ->groupBy('b.id')
+            ->having('step', '=', $step);
+        count($query->get());
+
+    }
+
+
+
 }
