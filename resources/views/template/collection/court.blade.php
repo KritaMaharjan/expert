a6156c599d82382e0697c341d4398cc999f5e760
sanjay <sanjay@alucio.com>
2015-05-27 17:46:02 -0700

    asdfasdf

--- resources/views/template/collection/court.blade.php
+++ resources/views/template/collection/court.blade.php
@@ -32,28 +32,18 @@
           <td colspan="2">{!! (isset($data['company_details']['logo']))? '<img src ="'.tenant()->folder('system')->url($data['company_details']['logo']).' "/>' : "<h1>Logo</h1>" !!}</td>
         </tr>
         <tr>
-          <td></td>
-          <td width="50%">
+          <td width="60%"></td>
+          <td width="40%">
               <table>
                 <tr>
                   <td><p><strong>Date:</strong> {{ date('d-m-y') }}<br />
                          <strong>Org.nr:</strong> {{ $data['company_details']['company_number'] }}<br />
-                         <strong>Telefon:</strong> {{ $data['company_details']['telephone'] }}
+                         <strong>Telefon:</strong> {{ $data['company_details']['telephone'] }}<br />
+                         <strong>Kontonummer:</strong> Account number<br />
+                         <strong>Saksbehandler:</strong> Username
                       </p>
                   </td>
-                  <td><p><strong>Due date:</strong> {{ format_date($data['due_date']) }}<br />
-                         <strong>Account no:</strong> {{ $data['company_details']['account_no'] }}<br />
-                         <strong>Case number:</strong> case number
-                      </p>
-                  </td>
-                </tr>
-                <tr>
-                  <td colspan="2"><p>Fullname: {{ $data['customer'] }}<br />
-                                     E-post: {{ $data['customer_details']['email'] }}<br />
-                                     {{--Website: Website--}}
-                                  </p>
-                  </td>
-                </tr>
+                </tr>               
               </table>
             </td>
         </tr>
@@ -62,11 +52,10 @@
       <br />
       <table>
         <tr>
-          <td><p>{{ $data['customer'] }}<br />
-                      {{ $data['customer_details']['street_name'] }} {{ $data['customer_details']['street_number'] }}<br />
-                      {{ $data['customer_details']['postcode'] }} {{ $data['customer_details']['town'] }}<br />
-                      {{--Country--}}</p>
-                    </td>
+          <td><p>Name of court<br />
+                 Street address<br />
+                 Town and postal code</p>
+          </td>
           {{--<td><P><b style="font-size:10px">Business name</b><br />
                     Street + Number<br />
                     Postalcode + Town<br />
@@ -80,91 +69,91 @@
 
       <table>
         <tr>
-          <td align="center"><h1 class="uppercase"><b>A request for conciliation proceedings</b></h1></td>              
+          <td align="center"><h1 class="uppercase"><b>Begjæring om forliksklage</b></h1></td>              
         </tr>
         <tr>
-          <td align="center">Disputes Act §6-3</td>
+          <td align="center">Tvisteloven §6-3</td>
         </tr><br /><br />
         <tr>
           <td>
             <table>              
               <tr>
-                <td>This requirement applies: </td>
+                <td>Kravet gjelder: </td>
               </tr>
               <tr>
-                <td>Principal invoice: {{ $data['invoice_number'] }} </td>
+                <td>Hovedstol faktura: {{ $data['invoice_number'] }} </td>
               </tr>
               <tr>
-                <td>Interest: 9.50% basis of bill DUE DATE {{ $data['due_date'] }} until payment</td>
+                <td>Renter: 9,50% ifraBILL {{ $data['due_date'] }} til betaling finner sted</td>
               </tr>
               <tr>
-                <td>Reminder fee: Fees,- {{ $data['remaining'] }}</td>
+                <td>Purregebyr: Fees,- {{ $data['remaining'] }}</td>
               </tr>
               <tr>
-                <td>Writing this petition: 860,-</td>
+                <td>Skriving av denne begjæring: 860,-</td>
               </tr>
               <tr>
-                <td>Court Fees Settlement Complaint: 860,-</td>
+                <td>Rettsgebyr Forliksklage: 860,-</td>
               </tr>
               <tr>
-                <td>Meeting fee: 430</td>
+                <td>Møtegebyr: 430</td>
               </tr>
               <tr>
-                <td><b>Total:</b> {{ $data['amount'] }} {{ $data['currency'] }} + Interest</td>
+                <td><b>Total:</b> {{ $data['amount'] }} {{ $data['currency'] }} + renter</td>
               </tr><br /><br /><br />
               <tr>
-                <td><b>Plaintiff:</b> {{ $data['company_details']['company_name'] }}<br/>{{ $data['company_details']['address'] }}</td>
+                <td><b>Saksøker:</b> {{ $data['company_details']['company_name'] }}, {{ $data['company_details']['address'] }}</td>
               </tr><br /><br />
               <tr>
-                <td><b>Defendant:</b> {{ $data['customer'] }}<br/>{{ $data['customer_details']['street_name'] }} {{ $data['customer_details']['street_number'] }}</td>
+                <td><b>Saksøkte:</b> {{ $data['customer'] }}, {{ $data['customer_details']['street_name'] }} {{ $data['customer_details']['street_number'] }}</td>
               </tr><br /><br /><br />
               <tr>
-                <td>It is submitted herewith a conciliation Appeals framot {{ $data['customer'] }}.</td>
+                <td>Det innleveres herved en Forliksklage framot {{ $data['customer'] }}.</td>
               </tr><br /><br /><br />
               <tr>
-                <td>Emphasis the claim following: </td>
+                <td>Det legges ned påstand om følgende:</td>
               </tr>
               <tr>
-                <td>Defendant sentenced within 14 days to pay. <b>Following:</b> </td>
+                <td>Saksøkt dømmes innen 14 dager å betale .<b>følgende:</b> </td>
               </tr><br /><br />
               <tr>
-                <td>Principal invoice: {{ $data['invoice_number'] }}</td>
+                <td>Hovedstol faktura: {{ $data['invoice_number'] }}</td>
               </tr>
               <tr>
-                <td>Interest: 9.50% advise the payment takes place</td>
+                <td>Renter: 9,50% ifra  til betaling finner sted</td>
               </tr>
               <tr>
-                <td>Reminder fee: {{ $data['remaining'] }} {{ $data['currency'] }}</td>
+                <td>Purregebyr: {{ $data['remaining'] }} {{ $data['currency'] }}</td>
               </tr>
               <tr>
-                <td>Writing this petition: 860</td>
+                <td>Skriving av denne Begjæring: 860</td>
               </tr>
               <tr>
-                <td>Court Fees Settlement Complaint: 860</td>
+                <td>Rettsgebyr Forliksklage: 860</td>
               </tr>
               <tr>
-                <td>Meeting fee: 430</td>
+                <td>Møtegebyr: 430</td>
               </tr>
               <tr>
-                <td>Total: {{ $data['amount'] }} {{ $data['currency'] }} + interest</td>
+                <td>Total: {{ $data['amount'] }} {{ $data['currency'] }} + renter</td>
               </tr>
               <tr>
-                <td>A brief report:</td>
+                <td>Kort redegjørelse:</td>
               </tr><br /><br />
               <tr>
                 <td>User defined text inserted here</td>
               </tr><br /><br /><br /><br />
               <tr>
-                <td>We ask to be under kit when conciliation meeting will be held.<br /> If the conditions are present or absent defendant required default trial.</td>
+                <td>Vi ber om å bli underettet om når Forliksmøtet avholdes.<br /> Dersom vilkårene er tilstede eller innklagede uteblir kreves avsagt uteblivelsesdom.</td>
               </tr><br /><br /><br />
               <tr>
-                <td>Yours for,</td>
+                <td>Med hilsen for,</td>
               </tr><br /><br /><br /><br />
               <tr>
                 <td>Business name</td>
               </tr><br /><br />
               <tr>
-                <td>Attachments</td>
+                <td>Vedlegg:</td>
               </tr>
               <tr>
                 <td>List of attachments</td>
